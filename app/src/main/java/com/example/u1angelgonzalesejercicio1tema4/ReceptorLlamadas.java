6585c34ed35c5204cce69372c80ee1a629119818
Gonzales <anggonzalesca@upt.pe>
2020-01-21 14:33:42 -0500

    Notificacion Llamada y SMS

--- app/src/main/java/com/example/u1angelgonzalesejercicio1tema4/ReceptorLlamadas.java
+++ app/src/main/java/com/example/u1angelgonzalesejercicio1tema4/ReceptorLlamadas.java
@@ -1,13 +1,90 @@
 package com.example.u1angelgonzalesejercicio1tema4;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import java.net.URLEncoder;
 
 public class ReceptorLlamadas extends BroadcastReceiver {
 
-    @Override public void onReceive(Context context, Intent intent) {
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "UNJBG";
 
+    @Override public void onReceive(Context context, Intent intent) {
+// Sacamos información de la intención
+        String estado = "", numero = "";
+        Bundle extras = intent.getExtras();
+
+        if (extras != null) {
+            estado = extras.getString(TelephonyManager.EXTRA_STATE);
+            if (estado.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                numero = extras.getString(
+                        TelephonyManager.EXTRA_INCOMING_NUMBER);
+                String info = "Ultima llamada recibida de " + ": " + numero;
+                Log.d("ReceptorAnuncio", info + " intent=" + intent);
+
+                //Funciones de cada opcion
+                Intent intencionLlamar = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:" + numero));
+                PendingIntent intencionPendienteLlamar =
+                        PendingIntent.getActivity(context,0, intencionLlamar,0);
+
+
+                Intent intencionmensajewhatsapp = new Intent(Intent.ACTION_VIEW);
+                try {
+
+                    String url = "https://api.whatsapp.com/send?phone=" + "+51" + numero + "&text="
+                            + URLEncoder.encode("Enviando texto ... ", "UTF-8");
+                    intencionmensajewhatsapp.setPackage("com.whatsapp");
+                    intencionmensajewhatsapp.setData(Uri.parse(url));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                PendingIntent intencionpendientewhatsapp = PendingIntent.getActivity(context,
+                        0, intencionmensajewhatsapp, 0);
+
+
+// Creamos Notificación
+                NotificationCompat.Builder notificacion = new
+                        NotificationCompat.Builder(context)
+                        .setContentTitle("Información de llamadas")
+                        .setContentText(info)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .addAction(android.R.drawable.ic_menu_call, "MENSAJE WHATSAPP", intencionpendientewhatsapp)
+                        .addAction(android.R.drawable.ic_menu_call, "LLAMAR", intencionPendienteLlamar)
+                        .setContentIntent(PendingIntent.getActivity(context, 0,
+                                new Intent(context, Servicio.class), 0));
+                NotificationManager notificationManager = (NotificationManager)
+                        context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel =
+                            new NotificationChannel(
+                                    NOTIFICATION_CHANNEL_ID,
+                                    NOTIFICATION_CHANNEL_NAME,
+                                    NotificationManager.IMPORTANCE_LOW);
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(R.color.colorAccent);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    notificacion.setChannelId(NOTIFICATION_CHANNEL_ID);
+                }
+                notificationManager.notify(ID_NOTIFICACION_CREAR, notificacion.build());
+                notificationManager.notify(1, notificacion.build());
+                //notificationManager.notify(0, notificacion.build());
+            }
+        }
     }
 }
--- app/src/main/java/com/example/u1angelgonzalesejercicio1tema4/ReceptorSMS.java
+++ app/src/main/java/com/example/u1angelgonzalesejercicio1tema4/ReceptorSMS.java
@@ -1,13 +1,73 @@
 package com.example.u1angelgonzalesejercicio1tema4;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class ReceptorSMS extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "UNJBG";
 
+    @Override public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, Servicio.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (bundle != null) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+                for (int j = 0; j < pdusObj.length; j++) {
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[j]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage(phoneNumber, null, message,null,null);
+
+                    NotificationCompat.Builder notificacion = new
+                            NotificationCompat.Builder(context)
+                            .setContentTitle("El número investigado es :" + senderNum)
+                            .setContentText(message)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentIntent(PendingIntent.getActivity(context, 0,
+                                    new Intent(context, Servicio.class), 0));
+                    NotificationManager notificationManager = (NotificationManager)
+                            context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        NotificationChannel notificationChannel =
+                                new NotificationChannel(
+                                        NOTIFICATION_CHANNEL_ID,
+                                        NOTIFICATION_CHANNEL_NAME,
+                                        NotificationManager.IMPORTANCE_LOW);
+                        notificationChannel.enableLights(true);
+                        notificationChannel.setLightColor(R.color.colorAccent);
+                        notificationManager.createNotificationChannel(notificationChannel);
+                        notificacion.setChannelId(NOTIFICATION_CHANNEL_ID);
+                    }
+                    notificationManager.notify(ID_NOTIFICACION_CREAR, notificacion.build());
+                    notificationManager.notify(1, notificacion.build());
+                    //notificationManager.notify(0, notificacion.build());
+
+                    Log.i("receptorsms", "senderNum: "+ senderNum + "; message: " + message);
+                    //Toast.makeText(context, "prueba:" + message,  Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+        }
     }
 }
